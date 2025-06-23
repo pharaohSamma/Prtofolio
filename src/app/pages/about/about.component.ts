8fb96abb7e5ca61c506551003e810b88f3957d93
pharaohSamma <mohyisaadani001@gmail.com>
2025-06-23 19:22:27 +0100

    fixing about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -11,32 +11,27 @@ export class AboutComponent {
       year: '2020',
       title: 'Started University',
       description: 'Began my journey in Computer Science.',
-      logo: 'assets/university.png',
     },
     {
       year: '2020',
       title: 'Started University',
       description: 'Began my journey in Computer Science.',
-      logo: 'assets/university.png',
     },
     {
       year: '2021',
       title: 'First Projects',
       description:
         'Built my first web applications and learned new technologies.',
-      logo: 'assets/icons/code.svg',
     },
     {
       year: '2022',
       title: 'Internship',
       description: 'Gained hands-on experience in software development.',
-      logo: 'assets/icons/internship.svg',
     },
     {
       year: '2024',
       title: 'Graduation',
       description: 'Successfully completed my degree in Computer Science.',
-      logo: 'assets/icons/graduation.svg',
     },
   ];
 }
